ddef267135ad9d36bc89a4188b2a3d78a9ca43e8
Yauheni Bryshten <zhenyabrishtenpl@gmail.com>
2024-10-08 05:20:27 +0200

    Added prometheus and grafana

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 ##########################################################
 ## Author: Yauheni Bryshten
 ##
-## This script create kubernetes cluster on AWS with
+## This script creates kubernetes cluster on AWS with
 ## 1 master and 1 worker t3.medium EC2 instances.
-## Then it deploys to Services and expose it to the
-## world using LoadBalancer.
+## Then it deploys two Services and expose it to the
+## world using Ingress.
 ##
 ## Prerequisites: Before the script, install kubectl and
 ## KOPS.
@@ -25,7 +25,7 @@
 # set -e # exit the script when error
 
 if (($# != 2)); then
-    echo "Wrong number of arguments"
+    echo "Wrong number of arguments. Use Cluster name and s3 bucket as parameters: ./deploy.sh \${NAME} \${KOPS_STATE_STORE}"
     exit 1
 fi
 
@@ -61,6 +61,15 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 rm -rf get_helm.sh
 
+# Add Prometheus and Grafana
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install prometheus prometheus-community/prometheus
+helm repo add grafana https://grafana.github.io/helm-charts
+helm install grafana grafana/grafana
+
+# Add nginx ingress controller
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
+
 # Add traefik ingress repo
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
@@ -68,8 +77,11 @@ helm install traefik traefik/traefik
 
 # Apply path based ingress
 kubectl apply -f ingress.yaml
+sleep 5
 
 # Usefull output at the end
 echo "Applications deployed"
-kubectl get svc | grep traefik | awk -F" " '{print "To access Django app use: " $4 "/django"}'
-kubectl get svc | grep traefik | awk -F" " '{print "To access Nginx app use: " $4 "/nginx"}' 
+
+kubectl get svc | grep "nginx-controller " | awk -F" " '{print "To access Grafana use: " $4 "/"}'
+kubectl get svc | grep "nginx-controller " | awk -F" " '{print "To access Django app use: " $4 "/django"}'
+kubectl get svc | grep "nginx-controller " | awk -F" " '{print "To access Nginx app use: " $4 "/nginx"}' 
